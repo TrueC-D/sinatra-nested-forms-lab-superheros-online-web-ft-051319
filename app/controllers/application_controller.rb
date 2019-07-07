f55d3c472c749379f00673be3b50107d80eb0144
Alethea Cary <aletheacary@gmail.com>
2019-07-07 18:31:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,3 +27,24 @@ class App < Sinatra::Base
     end
 
 end
+
+
+# From Valerie Moore to Everyone:  11:27 AM
+# class App < Sinatra::Base
+
+#     set :views, Proc.new { File.join(root, "../views/") }
+
+#     get '/' do
+#       erb :index
+#     end
+
+#     post "/teams" do
+#       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+#       members = params[:team][:members]
+#       @super_heroes = members.collect do |member_params|
+#         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+#       end
+#       erb :team
+#     end
+# end
+# here
